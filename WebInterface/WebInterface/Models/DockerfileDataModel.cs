bd6dd96e9bf943acf92842b24da035414b797a1d
DESKTOP-Q6BTA3G\peter <peter.kronsteiner@gmx.net>
2017-10-25 00:14:32 +0200

    Modify and download GAMS Dockerfile (first working Version, not finished yet)

---
+++ WebInterface/WebInterface/Models/DockerfileDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebInterface.Models
+{
+    public class DockerfileDataModel
+    {
+        public string LicencePath { get; set; }
+
+        public string Program { get; set; }
+
+        public string ProgramVersion { get; set; }
+
+        public string Model { get; set; }
+
+        public string ModelVersion { get; set; }
+
+        public string GitHubUser { get; set; }
+    }
+}
---
+++ WebInterface/WebInterface/Services/HomeControllerService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using WebInterface.Models;
+
+namespace WebInterface.Services
+{
+    public class HomeControllerService
+    {
+        public string GamsDockerFilePath { get; set; }
+
+        public void CreateGamsDockerfile(string licencePath = null, string outputFolder = "")
+        {
+            Debug.WriteLine("CreateGamsDockerfile");
+
+            string dockerfileContent;
+
+            const string dockerTemplate = @"./Docker-Templates/gams-dockerfile";
+
+            using (var reader = new StreamReader(dockerTemplate))
+            {
+                dockerfileContent = reader.ReadToEnd();
+            }
+
+            if (string.IsNullOrEmpty(licencePath))
+            {
+                dockerfileContent = dockerfileContent.Replace(@"COPY $GAMS_LICENSE /opt/gams/gamslice.txt", string.Empty);
+            }
+            else
+            {
+                licencePath = licencePath.Replace(@"\", "/");
+
+                dockerfileContent = dockerfileContent.Replace("$GAMS_LICENSE", licencePath);
+            }
+
+            try
+            {
+                if (string.IsNullOrEmpty(outputFolder))
+                {
+                    outputFolder = @"./Output/";
+                }
+
+                if (!Directory.Exists(outputFolder))
+                {
+                    Directory.CreateDirectory(outputFolder);
+                }
+
+                var outputfile = Path.Combine(outputFolder, "gams-dockerfile");
+                File.CreateText(outputfile);
+
+                File.WriteAllText(outputfile, dockerfileContent);
+
+                this.GamsDockerFilePath = outputfile;
+            }
+            catch (Exception ex)
+            {
+                //Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public string CreateDockerfile(DockerfileDataModel dockerfileDataModel)
+        {
+            return string.Empty;
+        }
+    }
+}
