9c6457cbe1a15b742dd5856bf500e0da509965c8
PeterKronsteiner <peter.kronsteiner@gmx.net>
2017-11-19 21:22:06 +0100

    WebRequests in C#, not finished yet

---
+++ WebInterface/Classes/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace WebInterface.Classes
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Models;
+
+    public static class JsonHelper
+    {
+        public static List<GithubRepository> GithubRepositoriesFromJson(string json) => JsonConvert.DeserializeObject<List<GithubRepository>>(json, Converter.Settings);
+
+        public static string GithubRepositoriesToJson(List<GithubRepository> self) => JsonConvert.SerializeObject(self, Converter.Settings);
+
+        public static List<GithubRepositoryVersion> GithubRepositoryVersionFromJson(string json) => JsonConvert.DeserializeObject<List<GithubRepositoryVersion>>(json, Converter.Settings);
+
+        public static string GithubRepositoryVersionToJson(List<GithubRepositoryVersion> self) => JsonConvert.SerializeObject(self, Converter.Settings);
+
+        public static GeoNodeDocument GeoNodeDocumentFromJson(string json) => JsonConvert.DeserializeObject<GeoNodeDocument>(json, Converter.Settings);
+
+        public static string GeoNodeDocumentToJson(GeoNodeDocument self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+}
---
+++ WebInterface/Models/GeoNodeDocument.cs
@@ -0,0 +1,78 @@
+﻿namespace WebInterface.Models
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public partial class GeoNodeDocument
+    {
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
+
+        [JsonProperty("objects")]
+        public List<Object> Objects { get; set; }
+    }
+
+    public partial class Object
+    {
+        [JsonProperty("abstract")]
+        public string Abstract { get; set; }
+
+        [JsonProperty("category__gn_description")]
+        public string CategoryGnDescription { get; set; }
+
+        [JsonProperty("csw_type")]
+        public string CswType { get; set; }
+
+        [JsonProperty("csw_wkt_geometry")]
+        public string CswWktGeometry { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("detail_url")]
+        public string DetailUrl { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("owner__username")]
+        public string OwnerUsername { get; set; }
+
+        [JsonProperty("popular_count")]
+        public long PopularCount { get; set; }
+
+        [JsonProperty("rating")]
+        public long Rating { get; set; }
+
+        [JsonProperty("share_count")]
+        public long ShareCount { get; set; }
+
+        [JsonProperty("srid")]
+        public string Srid { get; set; }
+
+        [JsonProperty("supplemental_information")]
+        public string SupplementalInformation { get; set; }
+
+        [JsonProperty("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("uuid")]
+        public string Uuid { get; set; }
+    }
+
+    public partial class Meta
+    {
+        [JsonProperty("limit")]
+        public long Limit { get; set; }
+
+        [JsonProperty("offset")]
+        public long Offset { get; set; }
+
+        [JsonProperty("total_count")]
+        public long TotalCount { get; set; }
+    }
+}
