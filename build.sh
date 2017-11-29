0f7f32c9d31b3bc7ed26d60980cd14a1db8db371
ptrkrnstnr <32832927+ptrkrnstnr@users.noreply.github.com>
2017-11-29 21:00:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 dotnet restore WebInterface/WebInterface.csproj
-dotnet publish ./WebInterface.sln -c Release -o ./obj/Docker/publish
+#dotnet publish ./WebInterface.sln -c Release -o ./obj/Docker/publish
 dotnet build WebInterface/WebInterface.csproj -c Release
